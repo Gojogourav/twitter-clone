29bb720d06ceb5a407a2c7a2a0fb03142b0c13b7
gojogourav <gojogourav@gmail.com>
2024-10-08 23:08:01 +0530

    updated post controller

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,127 +1,236 @@
-import { error } from 'console'
-import Post from '../models/post.model.js'
-import User from '../models/user.model.js'
-import { v2 as cloudinary } from 'cloudinary'
-import Notification from '../models/notification.model.js'
+import { error } from "console";
+import Post from "../models/post.model.js";
+import User from "../models/user.model.js";
+import { v2 as cloudinary } from "cloudinary";
+import Notification from "../models/notification.model.js";
+
+export const createPostProvider = async (req, res) => {
+  try {
+    const { text } = req.body;
+    let { img } = req.body;
+    const userId = req.user._id.toString();
+
+    const user = await User.findById(userId);
+
+    if (!user) return res.status(404).json({ message: "User not found!" });
+    if (!text && !img)
+      return res
+        .status(400)
+        .json({ message: "Post should have text or image!" });
+
+    if (img) {
+      const uploadedResponse = await cloudinary.uploader.upload(img);
+      img = uploadedResponse.secure_url;
+    }
+
+    const newPost = new Post({
+      user: userId,
+      text,
+      img,
+    });
+
+    await newPost.save();
+
+    res.status(201).json(newPost);
+  } catch (error) {
+    console.log(`Error happened at createPostProvider - ${error.message}`);
+
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const deletePostProvider = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) return res.status(404).json("Post not found");
+
+    if (post.user.toString() !== req.user._id.toString())
+      return res
+        .status(401)
+        .json({ error: "not authorized to delete this post" });
+
+    if (post.img) {
+      const imgId = post.img.split("/").split(".")[0];
+      await cloudinary.uploader.destroy(imgId);
+    }
+
+    await Post.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({ message: "Post deleted successfully" });
+  } catch (error) {
+    console.log(`Error occured at deletePostProvider - ${deletePostProvider}`);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const createCommentProvider = async (req, res) => {
+  try {
+    let { text, img } = req.body;
+    const postId = req.params.id;
+    const userId = req.user._id;
+
+    if (!text && !img) {
+      return res.status(400).json({ error: "no content is given" });
+    }
+
+    const post = await Post.findById(postId);
+
+    if (!post) return res.status(400).json({ error: "Post not found!" });
+
+    if (img) {
+      const uploadedResponse = await cloudinary.uploader.upload(img);
+      img = uploadedResponse.secure_url;
+    }
+
+    const comment = {
+      user: userId,
+      text,
+      img,
+    };
+    post.comments.push(comment);
+    await post.save();
+
+    res.status(200).json(post);
+  } catch (error) {
+    console.log(`Error in createCommentProvider ${error.message}`);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const likeUnlikePOstProvider = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const { id: postId } = req.params;
+
+    const post = await Post.findById(postId);
+
+    if (!post) return res.status(404).json({ error: "Post not found" });
+
+    const userLikedPost = post.likes.includes(userId);
+
+    if (userLikedPost) {
+      //unlike
+      await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
+      await User.updateOne({ _id: userId }, { $pull: { likedPosts: postId } });
+
+      res.status(200).json({ message: "Post unliked successfully" });
+    } else {
+      //like
+      post.likes.push(userId);
+      await User.updateOne({ _id: userId }, { $push: { likedPosts: postId } });
+
+      const notification = new Notification({
+        from: userId,
+        to: post.user,
+        type: "like",
+      });
+
+      await notification.save();
+
+      res.status(200).json({ message: "Post liked successfully" });
+    }
+  } catch (error) {
+    console.log(`Error occured in likeUnlikePOstProvider - ${error}`);
+    res.status(500).json({ error: Error.message });
+  }
+};
+
+export const getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find()
+      .sort({ createdAt: -1 })
+      .populate({
+        path: "user",
+        select: "-password",
+      })
+      .populate({
+        path: "comments.user",
+        select: "-password",
+      });
+
+    if (posts.length === 0) {
+      return res.status(200).json([]);
+    }
+
+    res.status(200).json(posts);
+  } catch (error) {
+    console.log(`Error in getAllPosts controller ${error}`);
+    res.status(200).json({ error: "Internal server error" });
+  }
+};
 
 
-export const createPostProvider = async(req,res)=>{
+export const getLikedPosts = async(req,res)=>{
+    const userId = req.params.id
     try{
-        const {text} = req.body
-        let {img} = req.body
-        const userId = req.user._id.toString()
-
         const user = await User.findById(userId)
-
-        if(!user) return res.status(404).json({message:"User not found!"})
-        if(!text && !img) return res.status(400).json({message:"Post should have text or image!"})
-
-        if(img){
-            const uploadedResponse = await cloudinary.uploader.upload(img)
-            img = uploadedResponse.secure_url
-        }
-
-        const newPost = new Post({
-            user:userId,
-            text,
-            img
+        if(!user) return res.status(404).json({error:"User not found!!!"})
+        
+        const likedPosts = await Post.find({_id:{$in:user.likedPosts}})
+        .populate({
+            path:"user",
+            select:"-password"
+        }).populate({
+            path:"comments.user",
+            select:"-password"
         })
-        
-        await newPost.save()
 
-        res.status(201).json(newPost)
+        res.status(200).json(likedPosts)
 
-    }catch(error){
-        console.log(`Error happened at createPostProvider - ${error.message}`);
-        
-        res.status(500).json({error:error.message})
+        }catch(error){
+        console.log(`Error in getLikedPostsprovider ${error}`);
+        res.status(500).json({error:"Internal error occured"})
     }
+
 }
 
-export const deletePostProvider = async(req,res)=>{
+export const getFollowingPosts = async(req,res)=>{
     try{
-        const post = await Post.findById(req.params.id)
-        if(!post) return res.status(404).json("Post not found")
-        
-        if(post.user.toString()!== req.user._id.toString()) return res.status(401).json({error:"not authorized to delete this post"})
-
-        if(post.img){
-            const imgId = post.img.split("/").split(".")[0]
-            await cloudinary.uploader.destroy(imgId)
-        }
-
-        await Post.findByIdAndDelete(req.params.id);
-
-        res.status(200).json({message:"Post deleted successfully"})
-
-    }catch(error){
-        console.log(`Error occured at deletePostProvider - ${deletePostProvider}`);
-        res.status(500).json({message:error.message})
-    }
-}
-
-export const createCommentProvider = async(req,res)=>{
-    try{
-        let {text,img} = req.body
-        const postId = req.params.id
         const userId = req.user._id
+        const user  = await User.findById(userId)
 
-        if(!text || !img) return res.status(400).json({error:"no content is given"})
+        if(!user) return res.status(404).json({error:"User not found"})
         
-        const post = await Post.findById(postId)
-        
-        if(!post) return res.status(400).json({error:"Post not found!"})
+        const following = user.following;
 
-        if(img){
-            const uploadedResponse = await cloudinary.uploader.upload(img)
-            img = uploadedResponse.secure_url
-        }
-        
-        const comment = {
-            user:userId,
-            text,img
-        }
-        post.comments.push(comment)
-        await post.save()
+        const feedPosts = await Post.find({user:{$in:following}})
+        .sort({createdAt:-1})
+        .populate({
+            path:"user",
+            select:"-password"
+        })
+        .populate({
+            path:"comments.user",
+            select:"-password"
+        })
 
-        res.status(200).json(post)
+        res.status(200).json(feedPosts)
+
     }catch(error){
-        console.log(`Error in createCommentProvider ${error.message}`);
+        console.log(`Error occured in getFollowingPosts controller ${error}`);
         res.status(500).json({error:error.message})
+
     }
 }
 
-export const likeUnlikePOstProvider = async(req,res)=>{
+export const getUserPosts = async(req,res)=>{
     try{
-        const userId = res.user._id
-        const {id:postId} = req.params
-
-        const post = await Post.findById(id)
-
-        if(!post) return res.status(400).json({error:error.message})
+        const {username} = req.params
+        const user = await User.findOne({username})
+        if(!user) return res.status(404).json({error:"User not found"})
         
-        const userLikedPost = post.likes.includes(userId)
-        
-        if(userLikedPost){
-            //unlike
-            await Post.updateOne({_id:postId},{$pull:{likes:userId}})
-            res.status(200).json({message:"Post unliked successfully"})
-        }else{
-            //like
-            await post.likes.push(userId)
-            res.status(200).json({message:"Post liked successfully"})
-            
-            const notification = new Notification({
-              from:userId,
-              to:  post.user,
-              type:"like"
-            })
+        const posts = await Post.find({user:user._id}).sort({createdAt:-1})
+        .populate({
+            path:"user",
+            select:"-password"
+        }).populate({
+            path:"comments.user",
+            select:"-password"
+        })
 
-            await notification.save()
-        }
-
+        res.status(200).json(posts)
     }catch(error){
-        console.log(`Error occured in likeUnlikePOstProvider`);
-        res.status(500).json({error:Error.message})
+        console.log(`Error occured in getUserPosts ${error}`);
+        res.status(500).json({error:error.message})
     }
 }
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,14 +1,17 @@
 import express from 'express'
 import { protectRoute } from '../middleware/protectRoute.js'
-import { createCommentProvider, createPostProvider, deletePostProvider } from '../controllers/post.controller.js'
+import { createCommentProvider, createPostProvider, deletePostProvider, getAllPosts, getFollowingPosts, getLikedPosts, getUserPosts, likeUnlikePOstProvider } from '../controllers/post.controller.js'
 
 const router = express.Router()
 
+router.get("/all",protectRoute,getAllPosts)
+router.get("/following",protectRoute,getFollowingPosts)
+router.get("/likes/:id",protectRoute,getLikedPosts)
 router.post("/create",protectRoute,createPostProvider)
 router.post("/comment/:id",protectRoute,createCommentProvider)
-// router.post("/like/:id",protectRoute,likeUnlikePost)
+router.post("/like/:id",protectRoute,likeUnlikePOstProvider)
 router.delete("/:id",protectRoute,deletePostProvider)
-
+router.get("/user/:username",protectRoute,getUserPosts)
 
 
 export default router
